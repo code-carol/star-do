85e497f84207b12008f1a47fd0ec2ad1d4a701bc
“crlnvls” <cveloso0808@gmail.com>
2024-01-11 16:52:47 +0000

    Add components to the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
 import "./Home.css";
+import Star from "../../components/Star";
+import Button from "../../components/Button";
+import Task from "../../components/Task";
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="title">star-do</h1>
+      <div className="center">
+        <Button className="btn-new-task">new task +</Button>
+        <Task>study Typescript</Task>
+        <Task>study for 30 min</Task>
+        <Task>create cute design</Task>
+      </div>
+      <Star left="5%" top="1%" />
+      <Star left="70%" top="10%" />
+      <Star left="10%" top="28%" />
+      <Star left="85%" top="40%" />
+      <Star left="30%" top="60%" />
+      <Star left="75%" top="80%" />
+      <Star left="20%" top="90%" />
     </>
   );
 };
