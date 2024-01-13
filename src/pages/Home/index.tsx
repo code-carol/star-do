9e7cb724dcd862b47578522bbc787db2812ec866
“crlnvls” <cveloso0808@gmail.com>
2024-01-13 23:15:52 +0000

    Add modal to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,28 @@ import "./Home.css";
 import Star from "../../components/Star";
 import Button from "../../components/Button";
 import Task from "../../components/Task";
+import Modal from "../../components/Modal";
+
+import { useState } from "react";
 
 const Home = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
   return (
     <>
+      {openModal && <Modal closeModal={setOpenModal} />}
       <h1 className="title">star-do</h1>
       <div className="center">
-        <Button className="btn-new-task">new task +</Button>
+        <Button className="btn-new-task" handleOnClick={handleOpenModal}>
+          new task +
+        </Button>
         <Task>study Typescript</Task>
         <Task>study for 30 min</Task>
-        <Task>create cute design</Task>
+        <Task>create cute design 1</Task>
       </div>
       <Star left="5%" top="1%" />
       <Star left="70%" top="10%" />
