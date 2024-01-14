c11fc33911697400ac37e5b89de400e181a837c8
“crlnvls” <cveloso0808@gmail.com>
2024-01-14 15:15:51 +0000

    Add  TaskList and handlers to Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,49 @@
 import "./Home.css";
 import Star from "../../components/Star";
 import Button from "../../components/Button";
-import Task from "../../components/Task";
 import Modal from "../../components/Modal";
+import TaskList from "../../components/TaskList";
+import { TaskType } from "../../types";
+import { v4 as uuidV4 } from "uuid";
 
 import { useState } from "react";
 
 const Home = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [tasks, setTasks] = useState<TaskType[]>([]);
 
   const handleOpenModal = () => {
     setOpenModal(true);
   };
 
+  const handleAddTask = (inputValue: string) => {
+    const newTask = {
+      id: uuidV4(),
+      title: inputValue,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
-      {openModal && <Modal closeModal={setOpenModal} />}
+      {openModal && <Modal closeModal={setOpenModal} addTask={handleAddTask} />}
       <h1 className="title">star-do</h1>
       <div className="center">
-        <Button className="btn-new-task" handleOnClick={handleOpenModal}>
+        <Button
+          className="btn-new-task"
+          handleOnClick={handleOpenModal}
+          type="button"
+        >
           new task +
         </Button>
-        <Task>study Typescript</Task>
-        <Task>study for 30 min</Task>
-        <Task>create cute design 1</Task>
+        <TaskList
+          taskItems={tasks}
+          handleDeleteTask={(id) => {
+            setTasks(tasks.filter((task) => task.id !== id));
+          }}
+        />
       </div>
       <Star left="5%" top="1%" />
       <Star left="70%" top="10%" />
