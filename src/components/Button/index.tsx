551a38bc17944820967f9319ba07196f4761b572
“crlnvls” <cveloso0808@gmail.com>
2024-01-14 15:13:57 +0000

    Add button types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,13 @@ interface BtnProps {
   className: string;
   children: string;
   handleOnClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  type: "submit" | "reset" | "button";
 }
 
-const Button = ({ className, children, handleOnClick }: BtnProps) => {
+const Button = ({ className, children, handleOnClick, type }: BtnProps) => {
   return (
     <>
-      <button className={className} onClick={handleOnClick}>
+      <button className={className} onClick={handleOnClick} type={type}>
         {children}
       </button>
     </>
