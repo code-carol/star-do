69ad3765c73d3b17fc5e38ab8991475821278fb8
“crlnvls” <cveloso0808@gmail.com>
2024-01-14 15:15:00 +0000

    Add a list and a delete handle to Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,19 +4,32 @@ import Button from "../Button";
 
 interface TaskProps {
   children: string;
+  onDelete: (id: string) => void;
+  id: string;
 }
 
-const Task = ({ children }: TaskProps) => {
+const Task = ({ children, onDelete, id }: TaskProps) => {
+  const handleDelete = () => {
+    onDelete(id);
+  };
   return (
     <>
-      <div className="container">
-        <label className="checkbox-label">
-          <input type="checkbox" value="task" />
-          <span className="check-box"></span>
-          <span className="label">{children}</span>
-          <Button className="btn-delete">x</Button>
-        </label>
-      </div>
+      <li>
+        <div className="container">
+          <label className="checkbox-label">
+            <input type="checkbox" value="task" />
+            <span className="check-box"></span>
+            <span className="label">{children}</span>
+            <Button
+              className="btn-delete"
+              handleOnClick={handleDelete}
+              type="button"
+            >
+              x
+            </Button>
+          </label>
+        </div>
+      </li>
     </>
   );
 };
