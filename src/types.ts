37e0049d6f75a9dae006ad03f9583c875e4ca4c2
“crlnvls” <cveloso0808@gmail.com>
2024-01-14 15:12:47 +0000

    Create a types file

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type TaskType = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+export type FormEvent = React.FormEvent<HTMLFormElement>;
+export type MouseEvent = React.MouseEvent<HTMLButtonElement>;
+export type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
